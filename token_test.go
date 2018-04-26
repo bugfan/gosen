80fb05e4656194ec1f7bdfb6f11cef22ae4ce134
Fan <30284615+bugfan@users.noreply.github.com>
2018-04-26 16:38:16 +0800

    all ok

--- token_test.go
+++ token_test.go
@@ -7,8 +7,9 @@ import(
 func TestSen(t *testing.T) {
 	
 	log.Println("-----测试-----")
-	obj:=New("*-")
-	obj.Tokens=[]string{"共产党","射精","阴茎","茎","阴道","VX","威信","裸","逼"}
-	log.Println(obj.Screening(`微信, 射精 asd9u按实际 !2VX`))
+	obj:=New("*")																// new一个对象(传入过滤之后的token)
+	obj.Fit=false																// 是否适配个数
+	obj.Tokens=[]string{"共产党","射精","阴茎","茎","阴道","VX","威信","裸","逼"}	// 设置敏感词库 
+	log.Println(obj.Screening(`微信, 射精 asd9u按实际 !2VX`))					  // 过滤
 	
 }
